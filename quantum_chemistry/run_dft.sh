3cecc24ff384fbf48bd18e3946cf9144cc57d657
Dominic Mashak <113135683+DominicMashak@users.noreply.github.com>
2025-10-21 02:17:09 -0500

    Update run_dft.sh

--- quantum_chemistry/run_dft.sh
+++ quantum_chemistry/run_dft.sh
@@ -3,7 +3,7 @@ python ~/Molecular-Evolution/quantum_chemistry/main.py \
     --functional B3LYP \
     --basis 6-31G \
     --method full_tensor \
-    --smiles "CN(NC=N)c1cn(C)c(=O)n(N)c1=N" \
+    --smiles "C1(-N)=C-C=C(-[N+](=O)-[O-])-C=C1" \
     --solvent none \
     --field-strength 0.001 \
     --properties beta dipole homo_lumo_gap transition_dipole oscillator_strength gamma energy alpha
