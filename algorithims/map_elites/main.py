279b05c12d46db469ae986b818640cf67cf96631
Jacob Schrum <schrum2@southwestern.edu>
2025-10-24 15:50:40 -0500

    Can create ME archive without crashing

--- algorithims/map_elites/main.py
+++ algorithims/map_elites/main.py
@@ -1,6 +1,13 @@
 
+import mapelites
 
 def main():
-    optimizer = MAPElites(
-        
+    optimizer = mapelites.MAPElitesArchive(
+        measure_dims = [10,10], 
+        measure_keys = ["num_atoms", "num_bonds"], 
+        objective_key = "rand"
     )
+    
+    
+if __name__ == "__main__":
+    main()
